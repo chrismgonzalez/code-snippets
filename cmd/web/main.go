862d77d80ef05d71f78e1a722b56bb0601f7d54c
chrismgonzalez <cmgonza89@gmail.com>
2020-07-22 20:09:54 -0500

    finish 4.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"cmgsoftware/snippetbox/pkg/models/mysql"
 	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 //application struct to hold app-wide dependencies
@@ -15,6 +15,7 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -38,6 +39,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	//http.Server struct
